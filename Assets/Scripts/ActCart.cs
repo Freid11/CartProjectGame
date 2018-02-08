894fb27a1d693e6feb3143d3e28de0bdc4f569ec
Librionius <librit@yandex.ru>
2018-02-08 16:26:16 +0300

    Карты
    Добавлены положение карт и привязка на камеру.

---
+++ Assets/Scripts/ActCart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActCart : MonoBehaviour {
+
+    private bool isCheck = false;
+    public GameObject Character;
+
+
+	void Update () {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (isCheck)
+            {
+                goCheckCart();
+            }
+            ClickMouse();
+            
+        }
+	}
+
+    void ClickMouse() {
+        RaycastHit rch = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out rch))
+        {
+            if (rch.collider.gameObject == gameObject)
+            {
+                goCheckCart();
+            }
+        }
+       
+    }
+
+    public void goCheckCart() {
+
+          if (isCheck)
+            {
+                gameObject.transform.position = gameObject.transform.position + Vector3.down;
+                isCheck = false;
+            }
+            else
+            {
+                gameObject.transform.position = gameObject.transform.position + Vector3.up;
+                isCheck = true;
+            }
+    }
+}
---
+++ Assets/Scripts/CartMassive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMassive : MonoBehaviour {
+    public GameObject[] Carts;
+    public GameObject CartPosition1;
+    public GameObject CartPosition2;
+    public GameObject CartPosition3;
+    public GameObject CartPosition4;
+    public GameObject CartPosition5;
+
+    void Start () {
+        GameObject cart = Instantiate(Resources.Load("Cart", typeof(GameObject))) as GameObject;
+        if (cart != null)
+        {
+            Instantiate(cart, CartPosition1.transform.position, CartPosition1.transform.rotation);
+        }
+        
+	}
+
+    public GameObject[] Get_MassiveCart()
+    {
+        GameObject[] Result = new GameObject[] { };
+        return Result;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
