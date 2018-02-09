534def75daa8aba377aec5cc7e451776f24bb8cf
Louco <dor2014spb@gmail.com>
2018-02-09 10:48:59 +0300

    comit

--- Assets/Script/Cart.cs
+++ Assets/Script/Cart.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Cart : MonoBehaviour {
 
+	Material tr;
+
 	// Use this for initialization
 	void Start () {
-		
+		tr = gameObject.GetComponent<MeshCollider>().material;
+		tr.color = Color.green;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
