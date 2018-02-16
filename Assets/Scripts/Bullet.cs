1afdcd307fd7932fef42de966638a0cf1d338f0b
Librionius <36152987+Librionius@users.noreply.github.com>
2018-02-16 22:32:56 +0300

    Много и по мелочи
    Сделал снаряд и попытался набросать башню. С башней мало что получилось кроме спрайта. Добавил уменьшение счетчика жизней при столкновении с снарядом. С обычными ближними атаками покане разобрался.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed { get; set; }
+    private Vector3 direction;
+    public Vector3 Direction { set { direction = value; } }
+    private SpriteRenderer sprite;
+    private int group;
+    public int Group { set { group = value; } }
+    /// <summary>
+    /// Физический урон от снаряда
+    /// </summary>
+    public int DamaPhysical { get; set; }
+    /// <summary>
+    /// Магический урон от снаряда
+    /// </summary>
+    public int DamaMagical { get; set; }
+    /// <summary>
+    /// Область поражения
+    /// </summary>
+    public float DamaSplash { get; set; }
+    /// <summary>
+    /// Граница поражения снаряда за этой границей он должен уничтожаться
+    /// </summary>
+    public float DamaRange { get; set; }
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        ///надо уничтожать обьект если он встретился с другим обьектом или если он дости границы зоны обстрела
+        Destroy(gameObject, 1.4f);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Character unit = collision.GetComponent<Character>();
+        if (unit && unit.GroupID !=group)///получается у нас может быть как один игрок так и пати, если у нас пати, то надо их групировать чтобы урон по союзникам не шел...или все таки делать урон по союзникам тоже? 
+        {
+            Destroy(gameObject);
+        }        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed + Time.deltaTime);
+	}
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,6 +11,7 @@ public class Character : MonoBehaviour {
 	public float SpeedMove;
 	public Sprite TextureCart;
 	public int Exp = 5;
+    public int GroupID;
 
 	[Header("Атака")]
 	[Range(10,1500)]
@@ -18,13 +19,15 @@ public class Character : MonoBehaviour {
 	[Range(10,1500)]
     public int atMagic;
     public int atCriticalDamage;
+    public float atCriticalDamageChance;
 	[Range(1,5)]
 	public float atRange;
     private SphereCollider colRangeAttack;
 	public bool atSplash;
+    public float atSplashRange;
 	[Range(10,100)]
 	public float atSpeed;
-	public TypeAttackEnum atType;
+	public RangeAttackEnum atType;
 
 	[Header("Защита")]
 	[Range(10,100)]
@@ -32,14 +35,55 @@ public class Character : MonoBehaviour {
 	[Range(10,100)]
     public int defMagic;
 
-	void Start(){
+    private Bullet bullet;
+    private Character AttackedEnemy; 
+
+	public void Start(){
 		
 	}
 
+    /// <summary>
+    /// Засчитывает урон персонажу от выстрела
+    /// </summary>
+    /// <param name="bullet">Выстрел от которого необходимо засчитать урон</param>
+    public void Receive_Damage(Bullet bullet)
+    {
+        Heals -= defMagic - bullet.DamaMagical;
+        Heals -= defPhysical - bullet.DamaPhysical;
+    }
+
+    private void Shoot()
+    {
+        Vector3 pos = transform.position;
+        
+        Bullet NewShoot = Instantiate(bullet,pos,bullet.transform.rotation)as Bullet;
+        NewShoot.DamaMagical = atMagic;
+        NewShoot.DamaPhysical = atPhysical;
+        NewShoot.DamaSplash = 
+        NewShoot.Group = GroupID;
+    }
+   
    	public void Awake(){
         colRangeAttack = GetComponent<SphereCollider>();
         colRangeAttack.radius = atRange;
+        bullet = Resources.Load<Bullet>("Bullet");
     }
 
-	public enum TypeAttackEnum{Near, Far};
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Bullet bullet = collision.GetComponent<Bullet>();
+        if (bullet)
+        {
+            Receive_Damage(bullet);
+        }
+    }
+
+    /// <summary>
+    /// Класс атаки
+    /// </summary>
+	public enum RangeAttackEnum{Near, Far};
+    /// <summary>
+    /// Тип атаки
+    /// </summary>
+    public enum TypeAttacEnum { Magic, Physical }
 }
